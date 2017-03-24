1deb367f76b1f02d094b868bdf74c81638817462
Paul Alexander <palexander@twitter.com>
2017-03-24 10:48:34 -0700

    Initial commit

---
+++ .local.bash
@@ -0,0 +1,87 @@
+# Add git branch support to shell
+unset GIT_PS1_SHOWDIRTYSTATE
+# unset GIT_PS1_SHOWUNTRACKEDFILES
+# unset GIT_PS1_SHOWSTASHSTATE
+# unset GIT_PS1_SHOWUPSTREAM
+
+function timer_start {
+  timer=${timer:-$SECONDS}
+}
+
+function timer_stop {
+  timer_show=$(($SECONDS - $timer))
+  unset timer
+}
+
+trap 'timer_start' DEBUG
+PROMPT_COMMAND=timer_stop
+
+if ! [ "$(type -t __git_ps1)" = function ]; then
+  function __git_ps1 {
+    "SUBSHELL"
+  }
+fi
+
+# function for setting terminal titles in OSX
+function title {
+  printf "\033]0;%s\007" "$1"
+}
+
+function my_git_ps1 {
+  PWD=$( pwd )
+  # if ! [[ $PWD =~ /source(/|$) ]]; then
+    __git_ps1
+  # fi
+}
+
+export PS1='\r\n${debian_chroot:+($debian_chroot)}\[\e[1;32m\]\u\[\e[1;30m\]@\h\[\e[0m\]: \[\e[0;36m\]\w \033[31m\]$("my_git_ps1")\[\033[00m\] \e[1;30m\]${timer_show}s\r\n\[\e[1;36m\]\$ \[\e[0;37m\]'
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -lFh'
+alias la='ls -A'
+alias l='ls -CF'
+alias ps='ps u'
+alias be='bundle exec'
+alias s='spring'
+alias srails='spring rails'
+alias srake='spring rake'
+alias spec='spring rspec'
+alias g='git'
+alias nbeg='NO_CI=1 beg'
+alias beg='NO_TIMEOUT=1 be guard start -P rspec -c'
+alias d='docker'
+alias dc='docker-compose'
+alias v='vagrant'
+
+export GPG_TTY=$(tty)
+
+export EDITOR="/usr/local/bin/subl -wn"
+export BUNDLER_EDITOR="/usr/local/bin/subl"
+export RAILS_ERRORS=1
+export NO_TIMEOUT=1
+export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
+# Don't show 'Will run ... ' messages
+export AUTOENV_QUIET=1
+
+RUBY_GC_HEAP_INIT_SLOTS=500000
+unset RUBY_HEAP_MIN_SLOTS
+unset RAILS_ENV
+unset CC
+unset VERSION
+
+export JAVA_LIBRARY_PATH=$JAVA_LIBRARY_PATH:/opt/twitter/lib
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(/opt/twitter/bin/jenv init -)"
+
+source /opt/twitter/opt/autoenv/activate.sh
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
---
+++ .pryrc
@@ -0,0 +1,27 @@
+begin
+  require 'awesome_print' 
+rescue LoadError => err
+  puts "no awesome_print :("
+end
+
+# Pry.config.correct_indent = false
+# Pry.config.auto_indent = false
+
+begin
+
+  if defined?(PryDebugger) || defined?( PryByebug )
+    Pry.commands.alias_command 'c', 'continue'
+    Pry.commands.alias_command 's', 'step'
+    Pry.commands.alias_command 'n', 'next'
+    Pry.commands.alias_command 'f', 'finish'
+    Pry.commands.alias_command 'wai', 'whereami'
+    Pry.commands.alias_command 'dp', 'disable-pry'
+  end
+
+rescue LoadError => err
+	puts err
+end
+
+Pry::Commands.command /^$/, "repeat last command" do
+  _pry_.run_command Pry.history.to_a.last  
+end  
